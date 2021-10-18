655be3b61dea24e4f07670ee414c675fea8eaca6
Stephanie J. Spielman <spielman@rowan.edu>
2021-10-18 16:54:25 -0400

    Paired programming

--- alignseq.py
+++ alignseq.py
@@ -2,35 +2,61 @@ import argparse
 import shutil
 import os
 
-parser = argparse.ArgumentParser(prog= 'Alignseq', description='Align Nucleotide Sequences', usage='%(prog)s [options]',epilog="And that's how you make an Alignment!")
-parser.add_argument('--inf', metavar='Infile', action='store', help='A input file of Nucleotides')
-parser.add_argument('--ouf', metavar='Outfile', action='store', help='A Output file (desired path) of Nucleotide Alignment')
-parser.add_argument('--prog', metavar='Program', action='store', help='Desired program to Align Sequences', default='mafft')
-#parser.add_argument('--act', metavar='Action', action='store', help='Action you want to do')
 
-args = parser.parse_args()
-infi = str(args.inf)
-outfi = str(args.ouf)
-pro = str(args.prog)
 
-class Vars:
-  def __init__(self, infile, outfile, program):
-      self.infile = infile
-      self.outfile = outfile
-      self.program = program
+def collect_input_arguments():
+    """
+    This stuff is documenting the function.
+    """
 
-test = Vars(infi, outfi, pro)
+    parser = argparse.ArgumentParser(prog= 'Alignseq', description='Align Codon Sequences', usage='%(prog)s [options]',epilog="And that's how you make an Alignment!")
+    parser.add_argument('--inf', metavar='Infile', action='store', help='A input file of codons')
+    parser.add_argument('--outf', metavar='Outfile', action='store', help='A Output file (desired path) of codon Alignment')
+    parser.add_argument('--prog', metavar='Program', action='store', help='Desired program to Align Sequences', default='mafft')
+    #parser.add_argument('--act', metavar='Action', action='store', help='Action you want to do')
+    
+    return parser
+    
+    
+class Settings:
+    def __init__(self, infile, outfile, program):
+        self.infile = infile
+        # before proceeding to define more stuff, make sure this definition is ok!
+        # or, define a bunch, and then check a bunch.
+        
+        
+        self.outfile = outfile
+        self.program = program
+
+    #def check_infile():
+        # Make sure the input file exists: os.path.exists(self.infile)
+    
+    
+    
+#class Sequences:
+# the sequence data, pre and post alignment (dictionaries encouraged)
+# functions to do translation and backtranslation
+# function to write final alignment to file  - very flexible with arguments!
+
+
+#class Aligner:
+# functions to perform the alignment
+# functions to assert executables are there
+#    def check_mafft(): SPIELMAN REMAINS UNSURE WHERE THIS SHOULD GO, you'll find the way by coding and something will make sense eventually
+#        # This function should define the mafft path (shutil.which) and do the assert
+    
+
+
+
 
 def main():
-    if test.program == "mafft":
-        program_path = shutil.which("mafft")
-        assert(program_path is not None) # make sure mafft is there!!
-        variables = " --quiet --preservecase "
-    if test.program == "placeholder":
-        program_path = shutil.which("placeholder")
-        assert(program_path is not None) # make sure placeholder is there!!
-    os.system(program_path + variables + test.infile + " > " + test.outfile)
-    os.system("cat "+ test.outfile)
+    
+    # Call a function to set up arguments and return an INSTANCE of the Settings class. Aka, a Settings object.
+    args = collect_input_arguments() 
+    #my_settings = Settings(args)
+
+    # Call a function to set up an INSTANCE (or just define an instance here) of a Sequences class. Aka, a Sequences object.
+
       
 main()
 
