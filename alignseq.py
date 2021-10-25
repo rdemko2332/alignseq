340c3d930cd9c83525b0f36e7820d9d8394709f0
Stephanie J. Spielman <spielman@rowan.edu>
2021-10-25 16:42:30 -0400

    Added fake nucleotide/codon file and paired programming to clean up the classes

--- alignseq.py
+++ alignseq.py
@@ -20,32 +20,40 @@ def collect_input_arguments():
 class Settings:
     def __init__(self, infile, outfile, program):
         self.infile = infile
-        # before proceeding to define more stuff, make sure this definition is ok!
-        # or, define a bunch, and then check a bunch.
         self.outfile = outfile
         self.program = program
+        
+        check_infile_exists()
+        check_program_exists()
+    
+    
+    def check_infile_exists(self):
+        assert os.path.exists(self.infile), "Path to infile does is incorrect."
+
+    def check_program_exists(self):
+        prog_path = shutil.which(self.program)
+        assert prog_path is not None, "Cannot find alignment software."
 
 
-def make_settings_class(infile, outfile, program):
-    my_settings = Settings(infile,outfile, program)
-    return(my_settings)
-
-def check_infile_exists(instance_infile):
-    result = os.path.exists(instance_infile)
-    return(result)
         
 class Sequences:
-    def __init__(self, instance_infile, instance_outfile):
-        self.infile = instance_infile
-        self.outfile = instance_outfile
-        self.translated = self.translate_data()
-    def translate_data(self):
-        with open(self.infile, "r") as file_handle:
+    def __init__(self):
+        # coming up eventually, not in init though!
+        # self.aligned = {}
+        # self.aligned_translated = {}
+        
+    def translate_data(self, infile):
+        with open(infile, "r") as file_handle:
             records = list(SeqIO.parse(file_handle, "fasta"))
-            dictionary = {}
+            self.unaligned_translated = {}
             for record in records:
-                dictionary[record.id] = record.seq.translate()
-            return(dictionary)
+                self.unaligned[record.id] = record.seq
+                self.unaligned_translated[record.id] = record.seq.translate()
+            
+            
+            
+        
+            
         # functions to do translation and backtranslation
 # function to write final alignment to file  - very flexible with arguments!
 
@@ -58,20 +66,15 @@ class Sequences:
 #/home/demkor62/Downloads/nuc.fasta
       
 def main():
-    # Call a function to set up arguments and return an INSTANCE of the Settings class. Aka, a Settings object.
     args = collect_input_arguments() 
-    my_settings = make_settings_class(args.inf, args.outf, args.prog)
-    if check_infile_exists(my_settings.infile) == True:
-        my_sequences = Sequences(my_settings.infile, my_settings.outfile)
-        print(my_sequences.translated)
-        if my_settings.program == 'mafft':
-            mafft_path = shutil.which("mafft")
-            os.system(mafft_path + " --quiet --preservecase " + my_settings.infile + " > " + my_settings.outfile)
-    else:
-        print('Input File Does Not Exist')
-
-    # Call a function to set up an INSTANCE (or just define an instance here) of a Sequences class. Aka, a Sequences object.
+    my_settings = Settings(args.inf, args.outf, args.prog)
     
+    my_sequences = Sequences()
+    my_sequences.translate_data(my_settings.infile)
+    
+    
+    #os.system(mafft_path + " --quiet --preservecase " + my_settings.infile + " > " + my_settings.outfile)
+
 main()
 
 
